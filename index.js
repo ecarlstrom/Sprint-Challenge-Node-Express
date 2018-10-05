418badb576c43f3b09fd0a883bc1ea8294f2086f
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-05 12:40:18 -0400

    project functions working properly, moving to getProjectActions() now

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ server.get('/api/actions', (req, res) => {
     console.log('\n** List of actions **', actions);
     res.json(actions);
   })
-  .catch(err => res.status(500).send({ error: "The list of actions could not be retrieved." }));
+  .catch(err => res.status(500).send({ error: "The list of actions could not be retrieved. </3 " }));
 });
 
 // add an action to the list
@@ -66,6 +66,61 @@ server.delete('/api/actions/:id', (req, res) => {
     .then(removedAction => {
       console.log(removedAction);
       res.status(200).json(removedAction);
+    })
+    .catch(err => console.error(err));
+});
+
+// projects
+
+// get all projects
+
+server.get('/api/projects', (req, res) => {
+  projectData.get().then(projects => {
+    console.log('\n** List of projects **', projects);
+    res.json(projects);
+  })
+  .catch(err => res.status(500).send({ error: "The list of projects could not be retrieved. </3 "}));
+});
+
+// add a project to the list
+
+server.post('/api/projects', (req, res) => {
+  const { id, name, description, completed } = req.body;
+  const newProject = { id, name, description, completed };
+  projectData.insert(newProject)
+    .then(id => {
+      projectData.findById(id)
+        .then(project => {
+          res.status(201).json(project);
+        });
+    })
+    .catch(err => res.status(500).send({ error: "The project could not be added to the database. </3 "}));
+});
+
+// update a project
+
+server.put('/api/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, description, completed } = req.body;
+  const newProject = { name, description, completed };
+
+  console.log(newProject);
+  projectData.update(id, newProject)
+    .then(project => {
+      console.log(project);
+      res.status(200).json(project);
+    })
+    .catch(err => console.error(err));
+});
+
+// remove a project
+
+server.delete('/api/projects/:id', (req, res) => {
+  const { id } = req.params;
+  projectData.remove(id)
+    .then(removedProject => {
+      console.log(removedProject);
+      res.status(200).json(removedProject);
     })
     .catch(err => console.error(err));
 });
